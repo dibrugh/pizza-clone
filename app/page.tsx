3e033f403ea41b49f490b5426fe08892c561f427
DiBrugh <dibrugh@gmail.com>
2024-08-22 12:46:02 +0200

    feat: implement TopBar component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { Container, Title, TopBar } from "@/components/shared";
 
 export default function Home() {
 	return (
-		<h1>
-			Home
-			<Button variant="outline">Корзина</Button>
-		</h1>
+		<>
+			<Container className="mt-10">
+				<Title text="Все пиццы" size="lg" className="font-extrabold" />
+			</Container>
+			<TopBar />
+		</>
 	);
 }
