276a63e8916fc60703b4487fc7e23aab61816609
DiBrugh <dibrugh@gmail.com>
2024-09-04 17:51:38 +0200

    feat: implement filtration logic

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -7,18 +7,15 @@ import {
 } from "@/shared/components/shared";
 import { prisma } from "@/prisma/prisma-client";
 import { Suspense } from "react";
+import { findPizzas } from "@/shared/lib";
+import { GetSearchParams } from "@/shared/lib/find-pizzas";
 
-export default async function Home() {
-	const categories = await prisma.category.findMany({
-		include: {
-			products: {
-				include: {
-					ingredients: true,
-					variants: true,
-				},
-			},
-		},
-	});
+export default async function Home({
+	searchParams,
+}: {
+	searchParams: GetSearchParams;
+}) {
+	const categories = await findPizzas(searchParams);
 
 	return (
 		<>
